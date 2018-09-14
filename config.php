311b2a029bcda42ad425ae3d587d030840e2d93f
schneiderbw <schneiderbw@gmail.com>
2018-09-14 14:12:11 -0400

    Initial Commit

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+//Application Information
+$companyname = "Honda North America";
+$eventname = "Honda Partnership Network 2018";
+$eventdatestring = "";
+$eventlocation = "";
+
+// MySQL Connection Information
+$sql_servername = "localhost";
+$sql_dbname = "hondarsvp2018";
+$sql_username = "hondarsvp2018";
+$sql_password = "l8ovo>11Zb?p'c_pXO1RCGbo|]jP5njp8)N<XB-\TU+U9ZOL";
+
+ ?>
---
+++ mysql.php
@@ -0,0 +1,36 @@
+<?php
+
+require 'config.php';
+
+//Redefine the variables we're getting from config.php
+$servername = $sql_servername;
+$username = $sql_username;
+$password = $sql_password;
+$dbname = $sql_dbname;
+
+//Create MySQL Connection
+
+$sql_conn = mysqli_connect($servername, $username, $password, $dbname);
+
+//Check connection
+
+if ($sql_conn->connect_error) {
+  die("Connection failed: " . $sql_conn->connect_error);
+}
+
+//mysql functions
+function dbRowInsert($table_name, $form_data)
+{
+    // retrieve the keys of the array (column titles)
+    $fields = array_keys($form_data);
+
+    // build the query
+    $sql = "INSERT INTO ".$table_name."
+    (`".implode('`,`', $fields)."`)
+    VALUES('".implode("','", $form_data)."')";
+
+    // run and return the query result resource
+    return mysqli_query($sql_conn, $sql);
+}
+
+ ?>
