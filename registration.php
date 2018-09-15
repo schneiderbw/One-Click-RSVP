ddedf011b4c2be62cbdabf7d7fe1deebb8eadfe2
schneiderbw <schneiderbw@gmail.com>
2018-09-15 14:42:14 -0400

    Bug Fixes

--- registration.php
+++ registration.php
@@ -4,7 +4,7 @@
 
   //Let's make the GET information friendlier to access
   $suppliertype = $_GET["type"];
-  $regemail = $_GET["email"];
+  $regemail = urldecode($_GET["email"]);
 ?>
 
 <?php
@@ -31,7 +31,7 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/application.css"
+    <link rel="stylesheet" href="css/application.css"></link>
   </head>
   <body>
     <?php if ($suppliertype == "tier1"): ?>
