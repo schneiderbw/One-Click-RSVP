0d96bada11668d55c9ae6c231cbe1bb8913da4b7
schneiderbw <schneiderbw@gmail.com>
2018-09-16 11:47:12 -0400

    Fix formatting

--- schedule.php
+++ schedule.php
@@ -108,13 +108,16 @@
     </div>
   </body>
   <script type="text/javascript">
-    function tConvert (time) {
+    function tConvert (intime) {
+      //Remove the last three characters from the string
+      newtime = intime.slice(0,-3);
+
       // Check correct time format and split into components
-      time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+      time = newtime.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
 
       if (time.length > 1) { // If time format correct
         time = time.slice (1);  // Remove full string match value
-        time[5] = +time[0] < 12 ? 'AM' : 'PM'; // Set AM/PM
+        time[5] = +time[0] < 12 ? ' AM' : ' PM'; // Set AM/PM
         time[0] = +time[0] % 12 || 12; // Adjust hours
       }
       return time.join (''); // return adjusted time or original string
