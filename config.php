e9b382ba11732a138c30d09a0cebd1138571dc7a
Benjamin W. Schneider <bschneider@techsoftsystems.com>
2019-09-06 14:07:04 -0400

    Changed config.php
    Modified config.php to reference a 'config' table hosted in SQL to store configuration details.  This will hopefully make the application easier to scale and maintain while securing the API information for Mailtrain

--- config.php
+++ config.php
@@ -1,12 +1,6 @@
 <?php
 
-//Application Information
-$companyname = "Honda North America";
-$eventname = "Honda Partnership Network 2018";
-$eventdatestring = "";
-$eventlocation = "";
-$headerlogo = "./assets/header.png";
-
+//Registration Closed/Open
 $diversereg_closed = "TRUE";
 $tier1reg_closed = "FALSE";
 
@@ -16,8 +10,24 @@ $sql_dbname = "hondarsvp2018";
 $sql_username = "hondarsvp2018";
 $sql_password = "l8ovo>11Zb?p'c_pXO1RCGbo|]jP5njp8)N<XB-\TU+U9ZOL";
 
-// Mailtrain API information
-$mailtrain_url = "https://mailtrain.techsoftsystems.com/api/"; // Full addres of the Mailtrain API including trailing slash
-$mailtrain_accesstoken = "REPLACE_ME!"; //API Access Token
+// *****DON'T MODIFY UNDER THIS LINE*****
+
+//Make SQL Connection
+include_once 'mysql.php';
+
+// Fetch config table as array for configuration variables
+$config_query = "SELECT configname, configproperty FROM config;";
+$config = mysqli_fetch_assoc(mysqli_query($sql_conn,$config_query));
+
+// Mailtrain API information (backwards compatibility)
+$mailtrain_url = $config['mt_url']; // Full addres of the Mailtrain API including trailing slash
+$mailtrain_accesstoken = $config['mt_apikey']; //API Access Token
+
+//Application Information
+$companyname = $config['companyname'];
+$eventname = $config['eventname'];
+$eventdatestring = $config['eventdatestring'];
+$eventlocation = $config['eventlocation'];
+$headerlogo = $config['headerlogo'];
 
  ?>
